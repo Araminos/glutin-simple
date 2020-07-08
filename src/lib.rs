18a4e04b1db901c1459585812d316accd013c4f2
K <kayo@illumium.org>
2020-07-08 17:19:00 +0500

    Android support

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-mod real_main;
+mod app;
+mod support;
+
+#[cfg(not(target_os = "android"))]
+pub use app::main;
 
 #[cfg(target_os = "android")]
-use crate::real_main::common_main;
-
-#[cfg(target_os = "android")]
-#[cfg_attr(target_os = "android", ndk_glue::main(backtrace))]
+#[ndk_glue::main(backtrace)]
 fn main() {
-    common_main();
+    app::main();
 }
-
-
